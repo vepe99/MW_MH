df8fca220bf07b7dcdc0159a9316b68b191a61bd
vepe99 <viterbogiuseppe99@gmail.com>
2024-03-06 23:02:01 +0100

    add try for pynbody.faceon error

--- tests/training_set_generation/traint_set_genartion.py
+++ tests/training_set_generation/traint_set_genartion.py
@@ -18,32 +18,38 @@ def extract_parameter_array(path='str'):
     sim = pb.load(path)
     sim.physical_units()
     h = sim.halos(write_fpos=False)
-    pb.analysis.angmom.faceon(h[1])
-    if len(sim.s['mass']) > 0:
-
+    try:
         regex = r'[^/]+$'
         name_file = re.search(regex, path).group()
-        name_parameter_file = '../../data/parameters/' + name_file + '_parameters.npz'
-        name_observable_file = '../../data/observables/' + name_file + '_observables.npz'
-
-        star_mass = sim.s['mass'].sum()
-        gas_mass = sim.g['mass'].sum()
-        dm_mass = sim.dm['mass'].sum()
-        infall_time = sim.properties['time'].in_units('Gyr')
-        redshift = sim.properties['z']
-        a = sim.properties['a'] 
-        chemical_mean = np.array([sim.s['metals'].mean(), sim.s['FeMassFrac'].mean(), sim.s['OxMassFrac'].mean()])
-        chemical_std = np.array([sim.s['metals'].std(), sim.s['FeMassFrac'].std(), sim.s['OxMassFrac'].std()])
-
-        np.savez(file=name_parameter_file, star_mass=star_mass, gas_mass=gas_mass, dm_mass=dm_mass, infall_time=infall_time, redshift=redshift, a=a, chemical_mean=chemical_mean, chemical_std=chemical_std)
-
-
-        feh = sim.s['feh']
-        ofe = sim.s['ofe']
-        np.savez(file=name_observable_file, feh=feh, ofe=ofe)
-
+        pb.analysis.angmom.faceon(h[1])
+    except:
+        np.savez(file='../../data/parameters/'+name_file+'_faceon_error.npz', emppty=np.array([0]))
+        np.savez(file='../../data/observables/'+name_file+'_faceon_error.npz', emppty=np.array([0]))
     else:
-        print('Not formed stars yet')
+        if len(sim.s['mass']) > 0:
+
+            name_parameter_file = '../../data/parameters/' + name_file + '_parameters.npz'
+            name_observable_file = '../../data/observables/' + name_file + '_observables.npz'
+
+            star_mass = sim.s['mass'].sum()
+            gas_mass = sim.g['mass'].sum()
+            dm_mass = sim.dm['mass'].sum()
+            infall_time = sim.properties['time'].in_units('Gyr')
+            redshift = sim.properties['z']
+            a = sim.properties['a'] 
+            chemical_mean = np.array([sim.s['metals'].mean(), sim.s['FeMassFrac'].mean(), sim.s['OxMassFrac'].mean()])
+            chemical_std = np.array([sim.s['metals'].std(), sim.s['FeMassFrac'].std(), sim.s['OxMassFrac'].std()])
+
+            np.savez(file=name_parameter_file, star_mass=star_mass, gas_mass=gas_mass, dm_mass=dm_mass, infall_time=infall_time, redshift=redshift, a=a, chemical_mean=chemical_mean, chemical_std=chemical_std)
+
+
+            feh = sim.s['feh']
+            ofe = sim.s['ofe']
+            np.savez(file=name_observable_file, feh=feh, ofe=ofe)
+
+        else:
+            print('Not formed stars yet')
+        
 
 all_paths = glob.glob('/mnt/storage/_data/nihao/nihao_classic/g*/0*/*.*.*')
 # Regular expression to match files that end with 5 numbers and don't have a dot at the end
@@ -55,5 +61,6 @@ regex = r'^.*\d{5}$' #all the snapshots
 paths = [path for path in all_paths if re.match(regex, path)]
 
 
-for path in tqdm(paths):
+for path in tqdm(paths[210:]):
+    print(path)
     extract_parameter_array(path)
